767730755cb9f73c452bd74e7403e3c806373e05
Coste <georgiana.coste@stud.ubbcluj.ro>
2021-10-12 21:50:46 +0300

    Am rezolvat problemele 10 si 13 din laboratorul 3.

---
+++ main.py
@@ -0,0 +1,149 @@
+from typing import List
+
+
+def show_menu():
+    print('1.Citire date.')
+    print('2.Determinare cea mai lunga secventa cu proprietatea ca toate numerele sunt pare.')
+    print('3.Determinare cea mai lunga secventa cu proprietatea ca toate numerele sunt formate din cifre prime.')
+    print('4.Iesire')
+
+
+def read_list() -> List[int]:
+    lst = []
+    lst_str = input('Scrieti numerele separandu-le printr-un spatiu.')
+    lst_str_split = lst_str.split(' ')
+    for numar in lst_str_split:
+        lst.append(int(numar))
+    return lst
+
+
+def get_longest_all_even(lst: List[int]) -> List[int]:
+    """
+    Determina cea mai lunga subsecventa in care toate elementele sunt pare.
+    Input:
+    -lst : Lista in care se cauta subsecventa
+    Output:
+    -subsecventa gasita
+    """
+    n = len(lst)
+    result = []
+    for st in range(n):
+        for dr in range(st, n):
+            all_even = True
+            for num in lst[st:dr + 1]:
+                if num % 2 != 0:
+                    all_even = False
+                    break
+            if all_even:
+                if dr - st + 1 > len(result):
+                    result = lst[st:dr + 1]
+    return result
+
+
+def get_is_prime(n):
+    """
+    Determina daca un numar este prim.
+    :param n: numarul care va fi verificat
+    :return: true daca numarul este prim, false daca numarul nu este prim
+    """
+    nr = 0
+    for div in range(2, n//2):
+        if n % div == 0:
+            nr = nr + 1
+    if nr == 0:
+        return True
+    else:
+        return False
+
+
+def get_all_prime_digits(m):
+    """
+    Determina daca un numar este format numai din cifre prime.
+    :param m: numarul care va fi verificat
+    :return: true daca numarul este format doar din cifre prime, false altfel
+    """
+    ok = 0
+    numar = m
+    while ok == 0 and numar != 0:
+        if not get_is_prime(int(numar % 10)):
+            ok = 1
+        numar = numar // 10
+    if ok == 0:
+        return True
+    else:
+        return False
+
+
+def get_longest_prime_digits(lst: List[int]) -> List[int]:
+    """
+    Determina cea mai lunga subsecventa care contine numerele formate din cifre prime.
+    :param lst: lista in care se cauta subsecventa
+    :return: subsecventa gasita.
+    """
+    n = len(lst)
+    result = []
+    for st in range(n):
+        for dr in range(st, n):
+            all_prime_digits = True
+            for num in lst[st:dr + 1]:
+                if not get_all_prime_digits(num):
+                    all_prime_digits = False
+                    break
+            if all_prime_digits:
+                if dr - st + 1 > len(result):
+                    result = lst[st:dr + 1]
+    return result
+
+
+def test_get_longest_all_even():
+    assert get_longest_all_even([1, 2, 4, 6, 5, 6, 4, 7, 9]) == [2, 4, 6]
+    assert get_longest_all_even([1, 3, 5, 8, 10, 11, 13]) == [8, 10]
+    assert get_longest_all_even([1, 2, 3, 5, 7, 9]) == [2]
+    assert get_longest_all_even([3, 5, 7]) == []
+
+
+def test_get_is_prime():
+    assert get_is_prime(3) == True
+    assert get_is_prime(6) == False
+    assert get_is_prime(7) == True
+    assert get_is_prime(11) == True
+    assert get_is_prime(12) == False
+
+
+def test_get_all_prime_digits():
+    assert get_all_prime_digits(135) == True
+    assert get_all_prime_digits(16) == False
+    assert get_all_prime_digits(371) == True
+    assert get_all_prime_digits(83) == False
+    assert get_all_prime_digits(36) == False
+
+
+def test_get_longest_prime_digits():
+    assert get_longest_prime_digits([83, 33, 65, 56, 800, 631]) == [33]
+    assert get_longest_prime_digits([33, 15, 57, 73, 88, 17]) == [33, 15, 57, 73]
+    assert get_longest_prime_digits([39, 293, 689, 66]) == []
+    assert get_longest_prime_digits([17, 66, 37, 71, 83, 80]) == [37, 71]
+
+
+def main():
+    lst = []
+    while True:
+        show_menu()
+        optiune = input('Optiunea: ')
+        if optiune == '1':
+            lst = read_list()
+        elif optiune == '2':
+            print('Cea mai lunga subsecventa cu toate numerele pare este:', get_longest_all_even(lst))
+        elif optiune == '3':
+            print('Cea mai lunga subsecventa cu toate numerele formate din cifre prime este:',
+                  get_longest_prime_digits(lst))
+        elif optiune == '4':
+            break
+
+
+if __name__ == '__main__':
+    test_get_longest_all_even()
+    test_get_is_prime()
+    test_get_all_prime_digits()
+    test_get_longest_prime_digits()
+    main()
